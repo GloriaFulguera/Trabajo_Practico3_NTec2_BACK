11debe9eeaf4c86a853dce5b897194972e4b9ceb
Gloria Fulguera <gloriafulguera@gmail.com>
2025-06-13 00:19:24 -0300

    Instalo paquetes swagger, implemento patron repository y conexion BD

--- Stock.Api/Program.cs
+++ Stock.Api/Program.cs
@@ -1,6 +1,32 @@
+using Stock.Services;
+using Stock.Services.Handlers;
+using Stock.Services.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder => {
+    builder.AllowAnyOrigin();
+    builder.AllowAnyMethod();
+    builder.AllowAnyHeader();
+}));
+
+SqliteHandler.ConnectionString = builder.Configuration.GetConnectionString("defaultConnection");
+builder.Services.AddSingleton<ICategoriaRepository, CategoriaService>();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stock.Backend");
+    c.RoutePrefix = string.Empty;
+});
+app.UseCors();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseAuthorization();
+app.UseAuthentication();
 app.Run();
--- Stock.Services/CategoriaService.cs
+++ Stock.Services/CategoriaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stock.Services.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Stock.Services
 {
-    public class CategoriaService
+    public class CategoriaService:ICategoriaRepository
     {
     }
 }
